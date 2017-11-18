52d6be31d6c02f6f07d1ec52fd6dc0be66a94518
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-11-17 19:17:32 -0500

    Add basic filtering functionality for catering interactive menu

--- views/catering/catering.js
+++ views/catering/catering.js
@@ -9,6 +9,53 @@ angular.module('myApp.catering', ['ngRoute'])
   });
 }])
 
-.controller('CateringCtrl', [function() {
+.controller('CateringCtrl', function($scope){
 
-}]);
+  $scope.tags = "";
+  $scope.filterList = [];
+
+  $scope.toggleFromFilterList = function(name) {
+    if ($scope.filterList.includes(name)) {
+      var index = $scope.filterList.indexOf(name);
+      $scope.filterList.splice(index, 1);
+    } else {
+      $scope.filterList.push(name);
+    }
+    $scope.tags = $scope.filterList.toString();
+  }
+
+  $scope.tag = function(item) {
+      if ($scope.tags) {
+        return $scope.tags.replace(/\s*,\s*/g, ',').split(',').every(function(tag) {
+          return item.tags.some(function(objTag){
+            return objTag.indexOf(tag) !== -1;
+          });
+        });
+      }
+      else {
+        return true;
+      }
+  };
+
+  $scope.menu = [
+    {
+      "name": "Falafel",
+      "desc": "Fresh falafel home made from scratch",
+      "price": 9,
+      "tags": ["hot", "vegetarian"]
+    },
+    {
+      "name": "Salad",
+      "desc": "Fresh salad home made from scratch",
+      "price": 7,
+      "tags": ["cold", "vegetarian"]
+    },
+    {
+      "name": "Chicken",
+      "desc": "Fresh portion of chicken",
+      "price": 6,
+      "tags": ["hot", "meat"]
+    }
+  ];
+
+});
