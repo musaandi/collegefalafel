194fac3103326006781e10141846c23344ec13ff
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-04-06 18:14:34 -0400

    Minor catering form udpates

--- views/catering/catering.js
+++ views/catering/catering.js
@@ -37,20 +37,58 @@ angular.module('myApp.catering', ['ngRoute'])
       }
   };
 
+  $scope.order = {
+    items: [],
+    subTotal: null,
+    total: null,
+    specialRequest: ""
+  }
+
+  $scope.addToOrder = function(item) {
+    $scope.order.items.push(item);
+    $scope.updateTotals();
+  }
+
+  $scope.removeFromOrder = function(id) {
+    var toDelete = null;
+    $scope.order.items.forEach(function(item, index){
+      if (item.id == id) {
+        toDelete = index;
+      }
+    });
+    if (toDelete) {
+      $scope.order.items.splice(index, 1);
+    }
+    $scope.updateTotals();
+  }
+
+  $scope.updateTotals = function() {
+    var orderItems = $scope.order.items;
+    var subTotal = 0;
+    orderItems.forEach(function(item){
+      subTotal += item.price;
+    })
+    $scope.order.subTotal = subTotal;
+    $scope.order.total = subTotal * 1.13;
+  }
+
   $scope.menu = [
     {
+      "id": 0,
       "name": "Falafel",
       "desc": "Fresh falafel home made from scratch",
       "price": 9,
       "tags": ["hot", "vegetarian"]
     },
     {
+      "id": 1,
       "name": "Salad",
       "desc": "Fresh salad home made from scratch",
       "price": 7,
       "tags": ["cold", "vegetarian"]
     },
     {
+      "id": 2,
       "name": "Chicken",
       "desc": "Fresh portion of chicken",
       "price": 6,
